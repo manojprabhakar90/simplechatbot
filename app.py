2665cfa8963272dd2f67b2af02b5108b5569423d
Manoj Prabhakar <manojprabhakar.isb@gmail.com>
2023-12-20 21:03:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,20 +33,46 @@ def bag_of_words(s, words):
 
 st.title("Candidtron Chatbot")
 
-user_input = st.text_input("You: ", key="input")
+tags = [intent['tag'] for intent in intents_data['intents'] if intent['responses']]
 
-if st.button("Send"):
+# Start a conversation list
+if 'conversation' not in st.session_state:
+    st.session_state['conversation'] = []
+
+# Display buttons for each tag
+for tag in tags:
+    if st.button(tag):
+        # Assume the first pattern is the representative one for the tag
+        pattern = next((item for item in intents_data['intents'] if item["tag"] == tag), None)['patterns'][0]
+        st.session_state['conversation'].append(f"You: {pattern}")
+        response = get_response(pattern)  # Assume this function generates the chatbot's response
+        st.session_state['conversation'].append(f"Candidtron: {response}")
+
+# Text input for free-form questions
+user_input = st.text_input("Ask me anything...", key="input")
+
+if st.button("Send", key="send"):
+    st.session_state['conversation'].append(f"You: {user_input}")
+    response = get_response(user_input)
+    st.session_state['conversation'].append(f"Candidtron: {response}")
+
+# Display the conversation
+for line in st.session_state['conversation']:
+    st.text(line)
+
+def get_response(user_input):
+    # Preprocess the user_input to create a bag-of-words
     bag = bag_of_words(user_input, words)
-    result = model.predict(np.array([bag]))[0]
-    result_index = np.argmax(result)
-    tag = labels[result_index]
+    
+    model_output = model.predict(np.array([bag]))[0]
+    predicted_tag_index = np.argmax(model_output)
+    predicted_tag = labels[predicted_tag_index]
 
-    response = "I didn't get that. Can you rephrase?"
-    if result[result_index] > 0.7:
-        for tg in data["intents"]:
-            if tg['tag'] == tag:
-                responses = tg['responses']
-                response = random.choice(responses)
-                break
+    responses = [intent['responses'] for intent in data['intents'] if intent['tag'] == predicted_tag]
+    
+    if model_output[predicted_tag_index] > 0.7:
+        response = random.choice(responses[0]) if responses else "I am not sure how to respond to that."
+    else:
+        response = "I didn't get that. Can you rephrase?"
 
-    st.text(f"Candidtron: {response}")
+    return response
